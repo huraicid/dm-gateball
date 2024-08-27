e3faf8d2fd1ec94696e6a577aaad8d7a3cd57014
huraicid <shu.tateuchi@gmail.com>
2024-08-27 21:01:20 +0900

    本番はDBに接続できるよう修正

--- src/main/java/dmgateball/TodoApplication.java
+++ src/main/java/dmgateball/TodoApplication.java
@@ -2,12 +2,19 @@ package dmgateball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class TodoApplication {
-
+@ComponentScan
+public class TodoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TodoApplication.class);
+	}
 }
